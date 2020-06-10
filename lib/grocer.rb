a5894ecdef664fdc6dc84c30e5a4920c13a5a409
Anh Vuong <vuonga1103@yahoo.com>
2020-06-10 15:11:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,15 +17,19 @@ def consolidate_cart(unconsolidated_cart)
   # Loop through each hash in unconsolidated_cart
   unconsolidated_cart.each do |unconsolidated_cart_item_hash|
     # Loop through each hash in result to see if there is a hash with the same item name as the current unconsolidated_cart_item_hash; if there is, add 1 to count
+    hash_found? = false
+    
     result.each do |result_item_hash|
       if result_item_hash[:item] == unconsolidated_cart_item_hash[:item]
         result_item_hash[:count] += 1
-        break
+        hash_found? = true
       end
     end
     # If finished looping through result, but there is no hash with the same item name as unconsolidated_cart_item_hash, add unconsolidated_cart_item_hash into result, and add count key with val of 1
-    unconsolidated_cart_item_hash[:count] = 1
-    result << unconsolidated_cart_item_hash
+    if hash_found? == false
+      unconsolidated_cart_item_hash[:count] = 1
+      result << unconsolidated_cart_item_hash
+    end
   end
   
   result
